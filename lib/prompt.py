6a523786dc08dc40753f5121bea1283c1c07f53e
atenliao <atenliao@gmail.com>
2023-09-10 00:30:16 -0700

    update edite worker gender

--- lib/prompt.py
+++ lib/prompt.py
@@ -38,8 +38,9 @@ class Prompt():
             1: "Female",
             2: "Maile"
         }
-        gender_key = self.confirm(self.makeMenu(options),input="Gender")
-        return self.makeMenu(options)
+        gender = self.makeMenu(options)
+        if self.confirm(gender,input="Gender"):
+            return gender
         # return options_dic.get(gender_key,"None")
 
     def getShift(self):
@@ -51,19 +52,9 @@ class Prompt():
         '6:30pm-11:30pm',
         '0:00am-5:00am'
         ]
-        # options_dic={
-        #     1: '7:30am-6:00pm', 
-        #     2: '7:30am-12:30pm', 
-        #     3: '1:00pm-6:00pm', 
-        #     4: '6:30pm-5:00am', 
-        #     5: '6:30pm-11:30pm',
-        #     6: '0:00am-5:00am'
-        # }
-        # default= '7:30am-6:00pm'
+       
         shift= self.makeMenu(options)
         if self.confirm(shift,input="time shift"):
-        #     shift_key = self.makeMenu(options)
-        # print('key: ',shift_key)
             return shift
         else:
             getShift()
